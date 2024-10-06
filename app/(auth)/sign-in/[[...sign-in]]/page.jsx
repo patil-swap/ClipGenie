35d91b5117e7077b291c9cc57a0b29b74af7d03c
Swapnil Patil <patil.k.swapnil@gmail.com>
2024-10-06 12:15:09 -0230

    Authentication using Clerk, sign-in and sign-up page setup

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -0,0 +1,21 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div>
+        <Image
+          className="w-full object-contain"
+          src={"/login.png"}
+          alt="login"
+          width={400}
+          height={400}
+        />
+      </div>
+      <div className="flex justify-center items-center h-screen">
+        <SignIn />
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div>AI Shorts</div>;
+  return (
+    <div>
+      AI Shorts
+      <UserButton />
+    </div>
+  );
 }
